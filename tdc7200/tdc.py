240b8679285960110b615304bdf94efd61e31579
yang RPI400 <jamyang838@icloud.com>
2024-03-21 16:34:47 +0800

    Refactor code to improve performance and readability

---
+++ tdc7200/tdc.py
@@ -0,0 +1,45 @@
+import spidev
+import RPi.GPIO as GPIO
+import time
+
+GPIO0=11
+# 使用標準的物理針位編號
+GPIO.setmode(GPIO.BOARD)
+# 設定 Pin 12 為 PWM 輸出
+# GPIO.setup(0, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(GPIO0, GPIO.OUT)
+# 將 Pin 11 設定為 LOW
+GPIO.output(GPIO0, GPIO.LOW)
+# time.sleep(1)
+GPIO.output(GPIO0, GPIO.HIGH)
+time.sleep(1)
+
+# 建立 SPI 物件
+spi = spidev.SpiDev()
+# 開啟 SPI 連接，0 表示 SPI port，1 表示 device (CS pin)
+spi.open(0, 1)
+# 設定 SPI 速度和模式
+spi.max_speed_hz = 10000000
+spi.mode = 0
+
+def read_reg(addr):
+    return spi.xfer2([addr, 0, 0,0,0,0,0])
+def write_reg(addr, data1,data2,data3):
+    return spi.xfer2([addr | 0x40, data1,data2,data3])
+
+
+# 設定 TDC7200 的設定值
+def getTime1():
+    a = write_reg(0,1,0,0)
+    while read_reg(0)[1] != 0:
+        pass
+    c = read_reg(0x10)
+    data = c[1] *65536 + c[2] * 256 + c[3]
+    print(data)
+
+while True:
+    getTime1()
+    time.sleep(1)
+# 關閉 SPI 連接
+spi.close()
+GPIO.cleanup()
